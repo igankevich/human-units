8257bde5204c2a817e6ce7629ac4e2ab1e12af56
Ivan Gankevich <ivan@igankevich.com>
2024-09-14 12:30:55 +0200

    CI

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,14 +2,6 @@
 
 . ./ci/preamble.sh
 
-install_token() {
-    cat >/dev/shm/credentials.toml <<EOF
-[repository]
-token = "$CRATES_IO_TOKEN"
-EOF
-    ln -sfn /dev/shm/credentials.toml "$CARGO_HOME"/credentials.toml
-}
-
 cargo_publish() {
     cargo publish --all-features --quiet
 }
@@ -18,5 +10,4 @@ cargo_publish() {
 #if test "$GITHUB_ACTIONS" = "true" && test "$GITHUB_REF_TYPE" != "tag"; then
 #    exit 0
 #fi
-install_token
 cargo_publish
