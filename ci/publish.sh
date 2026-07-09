e5e262e69e50763cd14736a2b27167bec7c0950e
Ivan Gankevich <ivan@igankevich.com>
2026-07-09 20:34:27 +0200

    Remove publishing from CI.

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,12 +2,5 @@
 
 . ./ci/preamble.sh
 
-cargo_publish() {
-    cargo publish --package human-units-derive --features serde --quiet
-    cargo publish --package human-units --features serde,si-units,iec-units,derive,std --quiet
-}
-
-if test "$GITHUB_ACTIONS" = "true" && test "$GITHUB_REF_TYPE" != "tag"; then
-    exit 0
-fi
-cargo_publish
+cargo publish --package human-units-derive --features serde --quiet
+cargo publish --package human-units --features serde,si-units,iec-units,derive,std --quiet
